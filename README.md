6a765fbe530eea819139d6ebee60d60b5b5b00c6
Eyal Posener <posener@gmail.com>
2017-11-24 12:45:49 +0200

    benchmark: add large struct tests

--- README.md
+++ README.md
@@ -112,14 +112,18 @@ $ go test -bench . -benchtime 10s
 goos: linux
 goarch: amd64
 pkg: github.com/posener/orm
-BenchmarkORMInsert-4    	 1000000	     22579 ns/op
-BenchmarkGORMInsert-4   	  200000	     68195 ns/op
-BenchmarkRawInsert-4    	 1000000	     19306 ns/op
-BenchmarkORMQuery-4     	    5000	   2878266 ns/op
-BenchmarkGORMQuery-4    	    2000	   9141819 ns/op
-BenchmarkRawQuery-4     	    5000	   2672683 ns/op
+BenchmarkORMInsert-4              	 1000000	     21634 ns/op
+BenchmarkGORMInsert-4             	  200000	     67917 ns/op
+BenchmarkRawInsert-4              	 1000000	     18546 ns/op
+BenchmarkORMQuery-4               	    5000	   2954333 ns/op
+BenchmarkGORMQuery-4              	    2000	   9880517 ns/op
+BenchmarkRawQuery-4               	    5000	   2672430 ns/op
+BenchmarkORMQueryLargeStruct-4    	    2000	   6741345 ns/op
+BenchmarkGORMQueryLargeStruct-4   	    1000	  20876834 ns/op
+BenchmarkRawQueryLargeStruct-4    	    2000	   7429866 ns/op
 PASS
-ok  	github.com/posener/orm	104.597s
+ok  	github.com/posener/orm	167.655s
+
 ```
 
 # Documentation
--- bench_test.go
+++ bench_test.go
@@ -3,13 +3,13 @@ package main_test
 import (
 	"database/sql"
 	"testing"
-
-	"github.com/posener/orm/example"
-	porm "github.com/posener/orm/example/personorm"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-
+	"github.com/posener/orm/example"
+	aorm "github.com/posener/orm/example/allorm"
+	porm "github.com/posener/orm/example/personorm"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -67,7 +67,7 @@ func BenchmarkRawInsert(b *testing.B) {
 // datasetSize is the number of columns that are used in querying benchmarks
 const datasetSize = 1000
 
-// BenchmarkORMQuery tests inserts with posener/orm package
+// BenchmarkORMQuery tests queries with posener/orm package
 func BenchmarkORMQuery(b *testing.B) {
 	db, err := sql.Open("sqlite3", ":memory:")
 	require.Nil(b, err)
@@ -91,7 +91,7 @@ func BenchmarkORMQuery(b *testing.B) {
 	}
 }
 
-// BenchmarkGORMQuery tests inserts with jinzhu/gorm package
+// BenchmarkGORMQuery tests queries with jinzhu/gorm package
 func BenchmarkGORMQuery(b *testing.B) {
 	db, err := gorm.Open("sqlite3", ":memory:")
 	require.Nil(b, err)
@@ -114,7 +114,7 @@ func BenchmarkGORMQuery(b *testing.B) {
 	}
 }
 
-// BenchmarkRawQuery tests inserts with raw SQL commands
+// BenchmarkRawQuery tests queries with raw SQL commands
 func BenchmarkRawQuery(b *testing.B) {
 	db, err := sql.Open("sqlite3", ":memory:")
 	require.Nil(b, err)
@@ -136,6 +136,89 @@ func BenchmarkRawQuery(b *testing.B) {
 		for rows.Next() {
 			var p example.Person
 			err := rows.Scan(&p.Name, &p.Age)
+			require.Nil(b, err)
+			ps = append(ps, p)
+		}
+		assert.Equal(b, datasetSize, len(ps))
+		rows.Close()
+	}
+}
+
+// BenchmarkORMQueryLargeStruct tests large struct queries with posener/orm package
+func BenchmarkORMQueryLargeStruct(b *testing.B) {
+	db, err := sql.Open("sqlite3", ":memory:")
+	require.Nil(b, err)
+	defer db.Close()
+
+	orm := aorm.New(db)
+
+	_, err = orm.Create().Exec()
+	require.Nil(b, err)
+
+	tm := time.Now().Round(time.Millisecond).UTC()
+
+	for i := 0; i < datasetSize; i++ {
+		_, err = orm.InsertAll(&example.All{String: "xxx", Select: i, Auto: i, Int: i, Time: tm, Bool: true}).Exec()
+		require.Nil(b, err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		alls, err := orm.Select().Query()
+		require.Nil(b, err)
+		assert.Equal(b, datasetSize, len(alls))
+	}
+}
+
+// BenchmarkGORMQueryLargeStruct tests large struct  queries with jinzhu/gorm package
+func BenchmarkGORMQueryLargeStruct(b *testing.B) {
+	db, err := gorm.Open("sqlite3", ":memory:")
+	require.Nil(b, err)
+	defer db.Close()
+
+	err = db.AutoMigrate(&example.All{}).Error
+	require.Nil(b, err)
+
+	tm := time.Now().Round(time.Millisecond).UTC()
+
+	for i := 0; i < datasetSize; i++ {
+		err = db.Create(&example.All{String: "xxx", Select: i, Auto: i, Int: i, Time: tm, Bool: true}).Error
+		require.Nil(b, err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var alls []example.All
+		err = db.Find(&alls).Error
+		require.Nil(b, err)
+		assert.Equal(b, datasetSize, len(alls))
+	}
+}
+
+// BenchmarkRawQueryLargeStruct tests large struct  queries with raw SQL commands
+func BenchmarkRawQueryLargeStruct(b *testing.B) {
+	db, err := sql.Open("sqlite3", ":memory:")
+	require.Nil(b, err)
+	defer db.Close()
+
+	_, err = db.Exec(`CREATE TABLE 'all' ( 'int' INT PRIMARY KEY, 'string' VARCHAR(100) NOT NULL, 'bool' BOOLEAN, 'auto' INT AUTO_INCREMENT, 'time' TIMESTAMP, 'select' INT )`)
+	require.Nil(b, err)
+
+	tm := time.Now().Round(time.Millisecond).UTC()
+
+	for i := 0; i < datasetSize; i++ {
+		_, err = db.Exec(`INSERT INTO 'all' ('int', 'string', 'bool', 'auto', 'time', 'select') VALUES (?, ?, ?, ?, ?, ?)`, i, "xxx", true, i, tm, i)
+		require.Nil(b, err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		rows, err := db.Query(`SELECT * FROM 'all'`)
+		require.Nil(b, err)
+		var ps []example.All
+		for rows.Next() {
+			var p example.All
+			err := rows.Scan(&p.Int, &p.String, &p.Bool, &p.Auto, &p.Time, &p.Select)
 			require.Nil(b, err)
 			ps = append(ps, p)
 		}
